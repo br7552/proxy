1c5c42401d4e1eaed0028b1a56b0369f7c560ac6
Blake <reyno254@unlv.nevada.edu>
2022-03-10 08:45:29 -0800

    handler with client that makes GET request with the address given in the path

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+var client http.Client
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		url := "http://" + r.URL.Path[1:]
+		resp, err := client.Get(url)
+		if err != nil {
+			fmt.Fprintf(w, "%v\n", err)
+			return
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(w, "%v\n", err)
+			return
+		}
+
+		fmt.Fprintf(w, "%s\n", body)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
