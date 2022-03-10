1d317874c841937ef50caa2e85a136490df775c8
Blake <reyno254@unlv.nevada.edu>
2022-03-10 12:54:13 -0800

    simple in-memory cache

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
+type cacheKey struct {
+	addr   string
+	method string
+}
+
+var cache = make(map[cacheKey]*http.Response)
 var client http.Client
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		url := "http://" + r.Host + r.URL.Path
-		req, err := http.NewRequest(r.Method, url, r.Body)
+		addr := "http://" + r.Host + r.URL.Path
+		if _, err := url.ParseRequestURI(addr); err != nil {
+			c := http.StatusBadRequest
+			http.Error(w, http.StatusText(c), c)
+			return
+		}
+
+		key := cacheKey{
+			addr:   addr,
+			method: r.Method,
+		}
+
+		if resp, ok := cache[key]; ok {
+			resp.Write(w)
+			return
+		}
+
+		req, err := http.NewRequest(r.Method, addr, r.Body)
 		if err != nil {
-			fmt.Fprintf(w, "proxy:%v\n", err)
+			log.Printf("proxy:%v\n", err)
 			c := http.StatusInternalServerError
 			http.Error(w, http.StatusText(c), c)
 			return
@@ -22,26 +43,17 @@ func main() {
 
 		resp, err := client.Do(req)
 		if err != nil {
-			fmt.Fprintf(w, "proxy:%v\n", err)
+			log.Printf("proxy:%v\n", err)
 			c := http.StatusInternalServerError
 			http.Error(w, http.StatusText(c), c)
 			return
 		}
 
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Fprintf(w, "proxy:%v\n", err)
-			c := http.StatusInternalServerError
-			http.Error(w, http.StatusText(c), c)
-			return
+		if r.Method == http.MethodGet || r.Method == http.MethodPost {
+			cache[key] = resp
 		}
 
-		for k, v := range resp.Header {
-			w.Header()[k] = v
-		}
-		w.WriteHeader(resp.StatusCode)
-		w.Write(body)
+		resp.Write(w)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
