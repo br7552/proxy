c8807c7d49b6524204e6b6a9360996cafe9d0603
Blake <reyno254@unlv.nevada.edu>
2022-03-11 11:29:51 -0800

    add terraform config

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {}
+
+resource "digitalocean_app" "proxy" {
+  spec {
+    name   = "proxy"
+    region = "nyc1"
+
+    service {
+      name               = "proxy-service"
+      instance_size_slug = "basic-xxs"
+
+      git {
+        branch         = "main"
+        repo_clone_url = "https://github.com/br7552/proxy"
+      }
+    }
+  }
+}
+
+output "live_url" {
+  value       = digitalocean_app.proxy.live_url
+  description = "The live URL of the proxy server."
+}
